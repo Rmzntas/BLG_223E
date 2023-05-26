f4d5a5543c104719ec224321ef52e076f46064eb
rmzntas <rmzntas08@gmail.com>
2023-05-27 00:21:06 +0300

    BTSNode

--- Homework_2/include/BSTNode.h
+++ Homework_2/include/BSTNode.h
@@ -1,3 +1,29 @@
 /* @Author
 Name: Ramazan Taş
 */
+
+class BSTNode{
+    private:
+
+        int chromo{};
+        int pos{};
+        char alt_base{};
+        BSTNode* left_child {nullptr};
+        BSTNode* right_child {nullptr};
+        BSTNode* parent {nullptr};
+
+    public:
+
+        BSTNode(int,int,char);
+        ~BSTNode();
+        int get_chromo() const;
+        int get_pos() const;
+        char get_alt_base() const;
+        BSTNode* get_parent()const;
+        BSTNode* get_leftchild() const;
+        BSTNode* get_rightchild() const;
+        void set_parent(BSTNode*);
+        void set_leftchild(BSTNode*);
+        void set_rightchild(BSTNode*);
+
+};
--- Homework_2/src/BSTNode.cpp
+++ Homework_2/src/BSTNode.cpp
@@ -1,3 +1,52 @@
 /* @Author
 Name: Ramazan Taş
 */
+
+#include"BSTNode.h"
+
+BSTNode::BSTNode(int chromo, int pos, char alt_base){
+    this->chromo = chromo;
+    this->pos = pos;
+    this->alt_base = alt_base;
+}
+
+BSTNode::~BSTNode(){
+    delete left_child;
+    delete right_child;
+}
+
+int BSTNode::get_chromo() const{
+    return chromo;
+}
+
+int BSTNode::get_pos() const{
+    return pos;
+}
+
+char BSTNode::get_alt_base() const{
+    return alt_base;
+}
+
+BSTNode* BSTNode::get_parent()const{
+    return parent;
+}
+
+BSTNode* BSTNode::get_leftchild()const{
+    return left_child;
+}
+
+BSTNode* BSTNode::get_rightchild()const{
+    return right_child;
+}
+
+void BSTNode::set_parent(BSTNode* parent){
+    this->parent = parent;
+}
+
+void BSTNode::set_leftchild(BSTNode* left){
+    this->left_child = left;
+}
+
+void BSTNode::set_rightchild(BSTNode* right){
+    this->right_child = right;
+}
