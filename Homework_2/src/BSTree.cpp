c8a2efc7f0ece274eafff5a934186a2d7e32438c
rmzntas <rmzntas08@gmail.com>
2023-05-28 23:20:31 +0300

    BST finding

--- Homework_2/src/BSTree.cpp
+++ Homework_2/src/BSTree.cpp
@@ -81,7 +81,49 @@ void BSTree::list() const{
 }
 
 void BSTree::finding(int chromo, int pos, char alt_base) const{
-    
+    BSTNode* temp = root;
+
+    while(true){
+        if(chromo < temp->get_chromo()){
+            if(temp->get_leftchild() == nullptr){
+               std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" could not be found"<<std::endl; 
+               break;       
+            }
+            temp = temp->get_leftchild();
+        }
+        else if(chromo > temp->get_chromo()){
+            if(temp->get_rightchild() == nullptr){
+                std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" could not be found"<<std::endl; 
+                break; 
+            }
+            temp = temp->get_rightchild();
+        }
+        else{   //chromo == temp.chromo
+            if(pos < temp->get_pos()){
+                if(temp->get_leftchild() == nullptr){
+                    std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" could not be found"<<std::endl; 
+                    break;       
+                }
+                temp = temp->get_leftchild();
+            }
+            else if( pos > temp->get_pos()){
+                if(temp->get_rightchild() == nullptr){
+                    std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" could not be found"<<std::endl; 
+                    break; 
+                }
+                temp = temp->get_rightchild();
+            }
+            else{   //pos == temp.pos
+                if(alt_base == temp->get_alt_base()){
+                    std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" was found"<<std::endl;
+                    break;
+                }
+                else{   //chromo == chromo , pos == pos, altbase != alt_base
+                    std::cout<<chromo<<" "<<pos<<" "<<alt_base<<" could not be found"<<std::endl;        
+                }
+            }
+        }
+    }
 }
 
 void BSTree::true_counter(BSTree* second) const{
