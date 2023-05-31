253ec3005d4e8df2832a331dbb8ddd265ed73c93
rmzntas <rmzntas08@gmail.com>
2023-05-31 16:13:24 +0300

    Avl Node

--- Homework_2/include/AVLNode.h
+++ Homework_2/include/AVLNode.h
@@ -1,3 +1,22 @@
 /* @Author
 Name: Ramazan Taş
 */
+
+class AVLNode{
+    private:
+        int chromo {};
+        int pos {};
+        char alt_base {};
+        
+    
+    public:
+        AVLNode(int, int, char);
+        ~AVLNode();
+        int get_chromo();
+        int get_pos();
+        char get_alt_base();
+        
+        AVLNode* left {nullptr};
+        AVLNode* right {nullptr};
+        int height;
+};
--- Homework_2/src/AVLNode.cpp
+++ Homework_2/src/AVLNode.cpp
@@ -1,3 +1,26 @@
 /* @Author
 Name: Ramazan Taş
 */
+
+#include"AVLNode.h"
+
+AVLNode::AVLNode(int chromo, int pos, char alt_base){
+    this->chromo = chromo;
+    this->pos = pos;
+    this->alt_base = alt_base;
+}
+
+AVLNode::~AVLNode(){
+    delete left;
+    delete right;
+}
+
+int AVLNode::get_chromo(){
+    return chromo;
+}
+int AVLNode::get_pos(){
+    return pos;
+}
+char AVLNode::get_alt_base(){
+    return alt_base;
+}
